9a3a0bdcb6ee7a8c978a0cfde0524f7084798740
{your_ramazanmammadov_goes_here} <{your_ramazanmammadov24@gmail.com_goes_here}>
2025-12-19 15:37:52 +0400

    .

---
+++ JS Task-01/script.js
@@ -0,0 +1,121 @@
+// Task-1
+console.log("My name is Murad");
+
+
+// Task-2.
+const name = 'Ramazan';
+
+console.log(`My name is ${name}`);
+
+
+// Task-3.
+// let person = "sdzexcfygjvj";
+// let console = "sedrftgyhujiko";
+// let $add = "xrcytvuhijk";
+
+
+// Task-4.
+let bookPrice = 750;
+let bookAmount = 14;
+
+console.log(bookPrice * bookAmount);
+
+
+// Task-5.
+let kmhSpeed = 220;
+let msSpeed = kmhSpeed / 3.6;
+
+console.log(msSpeed);
+
+
+// Task-6.
+
+
+
+
+// Task-7.
+let num = 17;
+
+let powerFive = num ** 5;
+
+let remainder = num % 3;
+
+console.log("Ədədin 5-ci dərəcə qüvvəti:", powerFive);
+console.log("3-ə bölünmədən qalan qalığı:", remainder);
+
+
+// Task-8.
+let firstName = "Alan";
+let lastName = "Turing";
+
+console.log(`Sizin adınız ${firstName} ${lastName}. Tanışlığımıza çox şadam!`);
+
+
+// Task-9.
+let Name = prompt("Zəhmət olmasa adınızı daxil edin:");
+
+console.log(`Salam, ${Name}!`);
+
+
+// Task-10.
+let num1 = prompt("Birinci ədədi daxil edin:");
+let num2 = prompt("İkinci ədədi daxil edin:");
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log("Toplama:", sum);
+console.log("Çıxma:", difference);
+console.log("Vurma:", product);
+console.log("Bölmə:", quotient);
+
+
+// Task-11.
+
+
+
+
+// Task-12.
+let age = prompt("Yaşınızı daxil edin:");
+
+age = Number(age);
+
+if (age < 16) {
+  alert("Access denied");
+} else {
+  alert("Access granted");
+}
+
+
+// Task-13.
+
+
+
+
+// Task-14.
+let id = prompt("enter product id:");
+
+switch (id) {
+  case "1":
+    alert("Available 10 pcs.");
+    break;
+  case "2":
+    alert("Available 256 pcs.");
+    break;
+  case "3":
+    alert("Available 53 pcs.");
+    break;
+  case "4":
+    alert("There are 3 available.");
+    break;
+  default:
+    alert("Out of stock");
+}
+
+
+// Task-15.
---
+++ JS Task-02/script.js
@@ -0,0 +1,71 @@
+// Task-1.
+let score = prompt("İmtahan nəticəsini daxil edin (0-100):");
+
+score = Number(score);
+
+let grade;
+if (score >= 90) {
+  grade = "A";
+} else if (score >= 80) {
+  grade = "B";
+} else if (score >= 70) {
+  grade = "C";
+} else if (score >= 60) {
+  grade = "D";
+} else {
+  grade = "F";
+}
+
+let comment;
+if (score > 85) {
+  comment = "əla";
+} else if (score >= 50 && score <= 85) {
+  comment = "yaxşı";
+} else {
+  comment = "kərs";
+}
+
+console.log("Qiymət:", grade);
+console.log("Şərh:", comment);
+
+
+// Task-2.
+let temp = prompt("Havanın temperaturunu daxil edin (°C):");
+
+temp = Number(temp);
+
+if (temp > 25) {
+  alert("Hava isti");
+} else if (temp >= 10 && temp <= 25) {
+  alert("Hava normal");
+} else if (temp >= 0 && temp < 10) {
+  alert("Hava soyuq");
+} else {
+  alert("Hava şaxtalı");
+}
+
+
+// Task-3.
+let num = prompt("Bir rəqəm daxil edin:");
+
+num = Number(num);
+
+if (num % 2 === 0) {
+  console.log(num + " cüt ədəddir.");
+} else {
+  console.log(num + " tək ədəddir.");
+}
+
+
+// Task-4.
+let username = "Ramazan";
+let password = "12345";
+
+let inputUsername = prompt("İstifadəçi adınızı daxil edin:");
+let inputPassword = prompt("Şifrənizi daxil edin:");
+
+if (inputUsername === username && inputPassword === password) {
+  console.log("Məlumatlar doğrudur. Giriş uğurludur!");
+} else {
+  console.log("Məlumatlar yalnışdır. Giriş uğursuzdur!");
+}

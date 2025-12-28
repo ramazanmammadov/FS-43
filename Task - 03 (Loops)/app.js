8587feee47397ccf221619cd286d90e4e8ab4a93
{your_ramazanmammadov_goes_here} <{your_ramazanmammadov24@gmail.com_goes_here}>
2025-12-28 17:27:33 +0400

    app

---
+++ Task - 03 (Loops)/app.js
@@ -0,0 +1,72 @@
+// Task-1.
+// for (i = +prompt("Iki reqemli eded daxil edin:"); i < 100; i+=7) {
+//     console.log(i)
+// }
+
+
+// Task-2.
+// let N = +prompt("Eded daxil edin:");
+
+// for (let i = 0; i < N; i++) {
+    //   console.log("I know how to use cycles");
+    // }
+    
+    
+// Task-3.    
+// for (let i = 100; i < 999; i +=10) {       
+//   console.log(i);
+// }
+
+
+// Task-4.
+// let sum = 0;
+
+// for (let i = 11; i <= 99; i += 2) {
+//   sum += i;
+// }
+// console.log(sum);
+
+
+// Task-5.
+// let N = +prompt("Eded daxil edin:");
+// let cem = 0;
+
+// for (let i = 100; i <= 999; i++) {
+//   if (i % N === 0) {
+//     cem += i;
+//   }
+// }
+
+// console.log(cem);
+
+
+// Task-6. 
+// let hasil = 1;
+
+// for (let i = 1; i <= 10; i++) {
+//   hasil *= i;
+// }
+
+// console.log(hasil);
+
+
+// Task-7.
+// let sum = 0;
+
+// for (let i = 1; i < 100; i+=2) {
+//     sum += i;
+// }
+
+// console.log(sum);
+
+
+// Task-13.
+// let num =+(prompt("Bir ədəd daxil edin:"));
+
+// let factorial = 1;
+
+// for (let i = 1; i <= num; i++) {
+//     factorial *= i;
+// }
+
+// console.log(factorial);

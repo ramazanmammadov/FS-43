141c0185d5f3be798cddb2a78b8c716774d9ec46
{your_ramazanmammadov_goes_here} <{your_ramazanmammadov24@gmail.com_goes_here}>
2026-01-07 13:32:04 +0400

    .

---
+++ JS Task - 04 (functions)/app.js
@@ -0,0 +1,94 @@
+// Task-1
+// function logGreeting(name) {
+//     console.log(`${name} is a JS developer`);
+// }
+
+// logGreeting("Ramazan")
+
+
+// Task-2
+// function logGreeting(userName) {
+//     console.log(`${userName} is active now`);
+// }
+
+// logGreeting(`Turan`)
+// logGreeting(`Ferid`)
+// logGreeting(`Ilkin`)
+
+
+// Task-3
+// function calcPrice(z,y,v) {
+//     let calc = z;
+
+//     if(y > calc) {
+//         calc = y;
+//     }
+
+//     else if(v > calc) {
+//         calc = v
+//     }
+    
+//     console.log(calc);
+// } 
+
+// calcPrice(23, 56, 31);
+// calcPrice(384, 782, 678);
+// calcPrice(-2, -7, -5);
+
+
+// Task-4
+// function calcDeposit(rubles, dollars, rate) {
+//   const total = rubles + (dollars * rate);
+//   console.log(`Bütün depozitlər üzrə məbləğ: ${total} rubl.`);
+// }
+
+// calcDeposit(1200, 20, 75);
+
+
+// Task-5
+// function min(a,b) {
+//     let num = a;
+
+//     if(b < num) {
+//         num = b;
+//     }
+
+//     console.log(num);
+// }
+
+// min(987,678);
+
+
+// Task-6
+// function concatenateStrings(text1, text2) {
+//   return text1 + text2;
+// }
+
+// const result = concatenateStrings("Salam ", "Müəllim!");
+// console.log(result);
+
+
+// Task-7
+// function calculateRectangleArea(length, width) {
+//   const area = length * width;
+
+//   console.log(area);
+// }
+
+// calculateRectangleArea(3, 4);
+
+
+// Task-8
+// function isEven(a) {
+//     if (a % 2 === 0) {
+//         console.log(`${a} Cüt ədəddir`);
+//     }
+//     else if (a % 2 === 1 || a % 2 === -1) {
+//         console.log(`${a} Tək ədəddir`);
+//     }
+//     else {
+//         console.log(`Zəhmət olmasa ədəd daxil edin!`);
+//     }
+// }
+
+// isEven(3478);

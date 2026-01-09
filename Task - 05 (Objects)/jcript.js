4e289c957a36e38e8f1aa36879d4f60d6fd73594
{your_ramazanmammadov_goes_here} <{your_ramazanmammadov24@gmail.com_goes_here}>
2026-01-09 10:12:22 +0400

    JavaScript files for Task 05

---
+++ Task - 05 (Objects)/jcript.js
@@ -0,0 +1,81 @@
+// Task-01
+// const user = {
+//     name: "Ramazan",
+//     surname: "Mammadov",
+//     gender: "male",
+//     birthday: "2004-08-24"
+// }
+
+// console.log(user.birthday);
+
+
+// Task-02
+// const user = {
+//     name: "Ramazan",
+//     surname: "Mammadov",
+//     gender: "male",
+//     birthday: "2004-08-24"
+// }
+
+// user.age = 21;
+// delete user.birthday;
+
+// console.log(user);
+
+
+// Task-03
+// const user = {
+//     name: "Ramazan",
+//     surname: "Mammadov"
+// }
+
+// for (const key in user) {
+    
+// console.log(key, ":", user[key]);    
+// }
+
+
+// Task-04
+// const user = {
+//     name: "Ramazan",
+//     surname: "Mammadov",
+//     gender: "male",
+//     birthday: "2004-08-24",
+//     setAge: function () {
+//         user.age = +prompt("yaşınızı daxil edin:");
+//     },
+//     getYearsBeforeRetirement: function () {
+//         const retirementAge = 65 - this.age;
+//         if (retirementAge > 0) {
+//             console.log(`Pensiya almaq üçün ${retirementAge} iliniz qalıb.`);
+//         } 
+//         else if (retirementAge <= 65) {
+//             console.log("Siz artıq pensiya yaşındasınız.");
+//         }
+//         else {
+//             console.log("Yanlış məlumat daxil etdiniz.");
+//         }
+//     }
+// }
+
+// user.setAge();
+// user.getYearsBeforeRetirement();
+// console.log(user);
+
+
+// Task-05
+// function prop2Taker(obj) {
+//     console.log(obj['prop-2']);   
+// }
+
+// prop2Taker({ one: 1, 'prop-2': 2});
+// prop2Taker({ 'prop-2': 'two', prop: 'test'});
+
+
+// Task-06
+
+
+
+
+
+// Task-07

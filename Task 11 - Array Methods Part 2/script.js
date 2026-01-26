a063e036df96752b4031b762102bef1c7d635aae
{your_ramazanmammadov_goes_here} <{your_ramazanmammadov24@gmail.com_goes_here}>
2026-01-26 19:07:26 +0400

    JavaScript files for Task 11 - Array Methods Part 2

---
+++ Task 11 - Array Methods Part 2/script.js
@@ -0,0 +1,58 @@
+// Task 1
+// const numbers = [5, 8, 7, 4, 10, 1, 21, 32, 2];
+// const max = Math.max(...numbers);
+
+// console.log(max);
+
+
+// Task-2
+// let employees = [["Jaylee Macy", "marketing"], ["John Smith", "management"], ["Blossom Hartley", "design"], ["Austin Carpenter", "marketing"], ["Joan Knowles", "development"], ["Sally Nunez", "management"], ["Laurel Ward", "development"], ["Lark Simon", "marketing"], ["Jane Stone", "management"], ["Courtney Olson", "development"],];
+// const development = employees.filter(employee => employee[1] === "development");
+
+// console.log(development);
+
+// // For dovru ile
+// const development = [];
+
+// for (let i = 0; i < employees.length; i++) {
+//   if (employees[i][1] === "development") {
+//     development.push(employees[i]);
+//   }
+// }
+// console.log(development);
+
+
+// Task-3
+// const array = [1, 6, 34, 5, 1, -4, 54, 76, 23, 65, 3, 50, 45, 100, 2346, -52, 6545, 0, 45, 22,];
+
+// const sortArray = (arr) => {
+//   const copiedArray = [...arr];       
+//   copiedArray.sort((a, b) => a - b);  
+//   return copiedArray;            
+// };
+// const sortedArray = sortArray(array);
+
+// console.log(sortedArray);
+
+
+// Task-4
+// const getNegativeNumbers = (numbers) => {
+//   const negativeNumbers = [];
+
+//   numbers.forEach(num => {
+//     if (num < 0) {
+//       negativeNumbers.push(num);
+//     }
+//   });
+//   if (negativeNumbers.length > 0) {
+//     return negativeNumbers;
+//   } 
+// };
+
+// const arr1 = [3, -1, 5, -7, 2];
+// const arr2 = [1, 4, -6, 9];
+
+// console.log(getNegativeNumbers(arr1)); 
+// console.log(getNegativeNumbers(arr2)); 
+
+
